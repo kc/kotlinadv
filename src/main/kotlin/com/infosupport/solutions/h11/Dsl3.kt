f9d34bb2b96f56551907fddd863d04714610c9e1
Bram Janssens <bram.janssens@infosupport.com>
2022-02-23 09:41:50 +0100

    Dsl3 solution added.

--- src/main/kotlin/com/infosupport/solutions/h11/Dsl3.kt
+++ src/main/kotlin/com/infosupport/solutions/h11/Dsl3.kt
@@ -1,46 +1,57 @@
 package com.infosupport.solutions.h11
 
-import com.infosupport.demos.h11.Country
-import com.infosupport.demos.h11.Customer
-import com.infosupport.demos.h11.Table
-import com.infosupport.demos.h7.conventions.Column
-
-// 3. How can we implement this query?
-fun query() {
-    (Country join Customer)
-        .select { Country.name eq "USA" }
-        .forEach { println(it) }
-}
-
-// - define the methods join and select on class ...?
-infix fun Table.join(other: Table): Table {
-    println("Table $table joined with ${other.table}.")
-    return Table("$table FULL OUTER JOIN ${other.table}")
-}
-
-// - select receives a lambda and returns a Query.
-// The lambda should have a receiver of type SqlExpressionBuilder.
-fun Table.select(where: SqlExpressionBuilder.() -> Boolean): Query {
-    return Query("SELECT * FROM $table WHERE ${where(SqlExpressionBuilder)}")
-}
-
-class Query(private val command: String = "") {
-    // - define forEach on class Query
-    fun forEach(function: (s: String) -> Unit) {
-        function(command)
-    }
-}
-
-// - SqlExpressionBuilder can be a singleton
-object SqlExpressionBuilder {
-    // - this way, we can define the infix method eq as member extension in SqlExpressionBuilder on Column.
-    infix fun <T> Column<T>.eq(value: T): Boolean {
-        println("${this.name} = $value?")
-        return this.name == value
-    }
-
-}
+import com.infosupport.demos.h11.assert
 
 fun main() {
-    query()
+    // 1. Implement this:
+    assert { 42 should be greaterThan 10 }
+    assert { 42 should be smallerThan 10 }
+
+    // 2. Is this also possible?
+    // not really possible: there should be an uneven amount of words for infix to succeed
+    assert { (42 should be).greaterAttempt thanAttempt 10 }
+
+    // possible: uneven amount of words
+    assert { 42 should be greater than number 10 }
 }
+
+// 1. ------------------------------------------
+
+object be
+
+infix fun Int.should(dummy: be) = BeWrapper(this)
+
+class BeWrapper(val value: Int) {
+
+    infix fun greaterThan(x: Int) {
+        if (value <= x) throw AssertionError(""""$value" is not greater than "$x"""")
+    }
+
+    infix fun smallerThan(x: Int) {
+        if (value >= x) throw AssertionError(""""$value" is not smaller than "$x"""")
+    }
+
+}
+
+// 2. ------------------------------------------
+
+// attempt:
+val BeWrapper.greaterAttempt get() = GreaterWrapper(value) // must be a property, and therefore cannot be infix
+
+// last word must be infix
+class GreaterWrapper(val value: Int) {
+    infix fun thanAttempt(i: Int) {
+        if (value <= i) throw AssertionError(""""$value" is not greater than "$i"""")
+    }
+}
+
+// better:
+object than
+object number
+
+infix fun BeWrapper.greater(dummy: than) = GreaterWrapper(value)
+
+infix fun GreaterWrapper.number(i: Int) {
+    if (value <= i) throw AssertionError(""""$value" is not greater than "$i"""")
+}
+
